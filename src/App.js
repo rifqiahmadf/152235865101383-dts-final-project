cda9aa817a3585b31ef71a330f6c35bf7e16d05b
rifqiahmadf <rahmadfauzi600@gmail.com>
2022-08-03 09:26:18 +0700

    change component's name SignIn and SignUp

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "fontsource-roboto";
 
 import "./App.css";
-import SignIn from "pages/SignIn";
-import SignUp from "pages/SignUp";
+import Login from "pages/Login";
+import Register from "pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/register" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
